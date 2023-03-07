73630664b84cd3e6f3753961619f2f409ed2466d
lws2269 <lws2269@kakao.com>
2023-03-07 17:32:37 +0900

    feat: TabBar 이미지, title 설정 및 화면전환 추가

--- OTT_Clone/OTT_Clone/TabBar/MainTabBarViewController.swift
+++ OTT_Clone/OTT_Clone/TabBar/MainTabBarViewController.swift
@@ -13,19 +13,36 @@ class MainTabBarViewController: UITabBarController {
         super.viewDidLoad()
         
         let homeVC = UIStoryboard(name: "Home", bundle: nil).instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
-        
+
         let newHotVC = UIStoryboard(name: "NewHot", bundle: nil).instantiateViewController(withIdentifier: "NewHotViewController") as! NewHotViewController
+        
+        homeVC.tabBarItem.title = "HOME"
+        homeVC.tabBarItem.image = UIImage(systemName: "house")
+        
+        newHotVC.tabBarItem.title = "NEW & HOT"
+        newHotVC.tabBarItem.image = UIImage(systemName: "play.rectangle.on.rectangle")
+        
+        self.viewControllers = [homeVC, newHotVC]
+        
+        // 탭바에 대한 설정
+        let tabBarAppeaarance = UITabBarAppearance()
+        tabBarAppeaarance.backgroundColor = .black
+        
+        // 탭바 아이템에 대한 설정
+        let tabBarItemAppearance = UITabBarItemAppearance()
+        tabBarItemAppearance.normal.iconColor = .darkGray
+        tabBarItemAppearance.normal.titleTextAttributes = [.foregroundColor : UIColor.darkGray]
+        tabBarItemAppearance.selected.iconColor = .white
+        tabBarItemAppearance.selected.titleTextAttributes = [.foregroundColor : UIColor.white]
+        
+        // 탭바 설정에 탭바 아이템 설정
+        tabBarAppeaarance.stackedLayoutAppearance = tabBarItemAppearance
+        tabBarAppeaarance.inlineLayoutAppearance = tabBarItemAppearance
+        tabBarAppeaarance.compactInlineLayoutAppearance = tabBarItemAppearance
+        
+        // 탭바에 탭바 설정
+        self.tabBar.standardAppearance = tabBarAppeaarance
+        self.tabBar.scrollEdgeAppearance = tabBarAppeaarance
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
