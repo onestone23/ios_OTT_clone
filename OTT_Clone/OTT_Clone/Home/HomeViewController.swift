a78f2abf6b5ebdfb5f9ac346335bf9802bf10e29
lws2269 <lws2269@kakao.com>
2023-03-07 13:29:40 +0900

    feat: HomeVC, TabBarVC 및 스토리보드 추가 와 폴더 구조 변경

--- OTT_Clone/OTT_Clone/Home/HomeViewController.swift
+++ OTT_Clone/OTT_Clone/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  OTT_Clone
 //
 //  Created by leewonseok on 2023/03/07.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
