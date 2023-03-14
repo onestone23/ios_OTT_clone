3e8f1a662d617455a2bfdd6e1fe9cf5699dd23c9
lws2269 <lws2269@kakao.com>
2023-03-14 14:56:12 +0900

    refactor: NewHotVC 네트워크 통신을 통한 tableView 데이터 표기

--- OTT_Clone/OTT_Clone/NewHot/NewHotCell/NewHotCell.swift
+++ OTT_Clone/OTT_Clone/NewHot/NewHotCell/NewHotCell.swift
@@ -28,18 +28,20 @@ class NewHotCell: UITableViewCell {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(systemName: "photo")
+        imageView.clipsToBounds = true
         return imageView
     }()
     
     var titleLabel: UILabel = {
         let label = UILabel()
-        label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     var descriptionLabel: UILabel = {
         let label = UILabel()
+        label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -53,9 +55,27 @@ class NewHotCell: UITableViewCell {
     var player = AVPlayer()
     var playerLayer = AVPlayerLayer()
     
+    var movieResult: MovieResult? {
+        didSet {
+            settingPlayerURL()
+            settingDate()
+            settingTitle()
+            settingDescription()
+            requestThumbnailImage()
+        }
+    }
+    
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.contentView.addSubview(baseContainerView)
+        baseContainerView.addSubview(moviewContainerView)
+        baseContainerView.addSubview(thumbnail)
+        baseContainerView.addSubview(dateLabel)
+        baseContainerView.addSubview(titleLabel)
+        baseContainerView.addSubview(descriptionLabel)
+        
         addBaseView()
         addMovieView()
         addMovieLayer()
@@ -77,9 +97,50 @@ class NewHotCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
     }
     
+    private func requestThumbnailImage() {
+        if let hasURL = movieResult?.artworkUrl {
+            NetworkLayer.request(urlString: hasURL) { image in
+                DispatchQueue.main.async {
+                    self.thumbnail.image = image
+                }
+            }
+            
+        }
+    }
+    
+    private func settingPlayerURL() {
+        if let previewURL = movieResult?.previewUrl,
+           let hasURL = URL(string: previewURL) {
+            self.player = AVPlayer(url: hasURL)
+            self.playerLayer.player = player
+            self.player.volume = 0
+            self.player.play()
+        }
+    }
+    
+    private func settingDate() {
+        if let hasDate = movieResult?.releaseDate {
+            let formatter = ISO8601DateFormatter()
+            
+            if let isoDate = formatter.date(from: hasDate) {
+                let myFormatter = DateFormatter()
+                myFormatter.dateFormat = "yyyy-MM-dd"
+                let dateString = myFormatter.string(from: isoDate)
+                
+                self.dateLabel.text = dateString
+            }
+        }
+    }
+    
+    private func settingTitle() {
+        self.titleLabel.text = movieResult?.trackName
+    }
+    
+    private func settingDescription() {
+        self.descriptionLabel.text = movieResult?.shortDescription
+    }
+    
     private func addBaseView() {
-        self.contentView.addSubview(baseContainerView)
-        
         NSLayoutConstraint.activate([
             baseContainerView.leftAnchor.constraint(equalTo: self.contentView.leftAnchor, constant: 50),
             baseContainerView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
@@ -89,13 +150,13 @@ class NewHotCell: UITableViewCell {
     }
 
     private func addMovieView() {
-        baseContainerView.addSubview(moviewContainerView)
-        
         NSLayoutConstraint.activate([
             moviewContainerView.topAnchor.constraint(equalTo: baseContainerView.topAnchor),
             moviewContainerView.leftAnchor.constraint(equalTo: baseContainerView.leftAnchor),
             moviewContainerView.rightAnchor.constraint(equalTo: baseContainerView.rightAnchor),
             moviewContainerView.heightAnchor.constraint(equalToConstant: 200),
+            moviewContainerView.bottomAnchor.constraint(equalTo: thumbnail.topAnchor, constant: -10),
+            
         ])
     }
     
@@ -107,41 +168,31 @@ class NewHotCell: UITableViewCell {
     }
     
     private func addThumbnail() {
-        baseContainerView.addSubview(thumbnail)
-        
         NSLayoutConstraint.activate([
-            thumbnail.topAnchor.constraint(equalTo: moviewContainerView.bottomAnchor, constant: 10),
             thumbnail.leftAnchor.constraint(equalTo: baseContainerView.leftAnchor),
             thumbnail.heightAnchor.constraint(equalToConstant: 80),
+            thumbnail.widthAnchor.constraint(equalToConstant: 50),
+            thumbnail.bottomAnchor.constraint(equalTo: dateLabel.topAnchor, constant: -10),
         ])
     }
     
     private func addDateLabel() {
-        baseContainerView.addSubview(dateLabel)
-        
         NSLayoutConstraint.activate([
-            dateLabel.topAnchor.constraint(equalTo: thumbnail.bottomAnchor, constant: 10),
             dateLabel.leftAnchor.constraint(equalTo: baseContainerView.leftAnchor),
+            dateLabel.bottomAnchor.constraint(equalTo: titleLabel.topAnchor, constant: -10),
         ])
-        
     }
     
     private func addTitleLabel() {
-        baseContainerView.addSubview(titleLabel)
-        
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: dateLabel.bottomAnchor),
             titleLabel.leftAnchor.constraint(equalTo: baseContainerView.leftAnchor),
             titleLabel.rightAnchor.constraint(equalTo: baseContainerView.rightAnchor, constant: -20),
+            titleLabel.bottomAnchor.constraint(equalTo: descriptionLabel.topAnchor, constant: -10)
         ])
-        
     }
     
     private func addDescriptionLabel() {
-        baseContainerView.addSubview(descriptionLabel)
-        
         NSLayoutConstraint.activate([
-            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
             descriptionLabel.leftAnchor.constraint(equalTo: baseContainerView.leftAnchor),
             descriptionLabel.rightAnchor.constraint(equalTo: baseContainerView.rightAnchor, constant: -20),
             descriptionLabel.bottomAnchor.constraint(equalTo: baseContainerView.bottomAnchor, constant: -30)
--- OTT_Clone/OTT_Clone/NewHot/NewHotViewController.swift
+++ OTT_Clone/OTT_Clone/NewHot/NewHotViewController.swift
@@ -11,28 +11,44 @@ class NewHotViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var movieModel: MovieModel? {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.rowHeight = UITableView.automaticDimension
         tableView.register(NewHotCell.self, forCellReuseIdentifier: "NewHotCell")
+        
+        NetworkLayer.request(type: .movie) { model in
+            self.movieModel = model
+        }
     }
 }
 
 extension NewHotViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return movieModel?.resultCount ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "NewHotCell", for: indexPath) as? NewHotCell else {
             return NewHotCell()
         }
+        
+        let movieResult = self.movieModel?.results[indexPath.row]
+        cell.movieResult = movieResult
+    
         return cell
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 300
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
     }
-
 }
